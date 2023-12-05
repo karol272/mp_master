9c686093763d2a317c2d314960cb322c9c635bb0
karol272 <49593087+karol272@users.noreply.github.com>
2023-12-04 19:32:36 -0500

    Agregar
    Archivos de inicio

---
+++ myfile.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 19:29:17 2023
+
+@author: dcromeroc
+"""
+
+"""myfile2.py"""
+A= 7 +9
+print("A" ,A)
+print("agregar algunas lineas mas")
+print("agregar algunas lineas mas")
+print("agregar algunas lineas mas")
+B= 5 +2 
+print("B" ,B)
+print("agregar algunas lineas mas")
